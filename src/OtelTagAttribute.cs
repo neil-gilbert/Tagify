f7b08e24e3dddfd22eb2b6e784ed44f246b6880b
Neil Gilbert <neil_gilbert_1234@hotmail.com>
2024-09-07 17:03:33 +0100

    Update OtelTagAttribute.cs

--- src/OtelTagAttribute.cs
+++ src/OtelTagAttribute.cs
@@ -1,32 +1,29 @@
-using System;
+namespace OtelTagify;
 
-namespace OtelTagify
+/// <summary>
+/// Attribute to specify OpenTelemetry tag details for a property.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class OtelTagAttribute : Attribute
 {
     /// <summary>
-    /// Attribute to specify OpenTelemetry tag details for a property.
+    /// Gets the name of the tag.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
-    public class OtelTagAttribute : Attribute
+    public string Name { get; }
+
+    /// <summary>
+    /// Gets the prefix for the tag.
+    /// </summary>
+    public string? Prefix { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OtelTagAttribute"/> class.
+    /// </summary>
+    /// <param name="name">The name of the tag.</param>
+    /// <param name="prefix">The prefix for the tag.</param>
+    public OtelTagAttribute(string name, string? prefix = null)
     {
-        /// <summary>
-        /// Gets the name of the tag.
-        /// </summary>
-        public string Name { get; }
-
-        /// <summary>
-        /// Gets the prefix for the tag.
-        /// </summary>
-        public string Prefix { get; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="OtelTagAttribute"/> class.
-        /// </summary>
-        /// <param name="name">The name of the tag.</param>
-        /// <param name="prefix">The prefix for the tag.</param>
-        public OtelTagAttribute(string name, string prefix = null)
-        {
-            Name = name;
-            Prefix = prefix;
-        }
+        Name = name;
+        Prefix = prefix;
     }
 }
