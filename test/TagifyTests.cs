677203775cee8098b6a126080432787be43ed016
Neil Gilbert <neil_gilbert_1234@hotmail.com>
2024-09-10 20:14:56 +0100

    Update TagifyTests.cs

--- test/TagifyTests.cs
+++ test/TagifyTests.cs
@@ -1,75 +1,67 @@
 using System.Diagnostics;
+using Xunit;
 
 namespace Tagify.Tests;
 
 public class TagifyTests
 {
     [Fact]
-    public void SetTagsFromObject_WithTaggedProperties_ShouldSetCorrectTags()
+    public void AddTagsForUserInfo_ShouldAddCorrectTags()
     {
-        // Arrange
-        var testObject = new TestClass
+        var user = new UserInfo
         {
-            TaggedProperty = "test value",
-            PrefixedProperty = 42,
-            UntaggedProperty = "should not be tagged"
+            Id = 123,
+            Name = "John Doe",
+            Email = "john@example.com"
         };
-
         var activity = new Activity("TestActivity");
         activity.Start();
 
-        // Act
-        activity.SetTagsFromObject(testObject);
+        activity.AddActionTagsForUserInfo(user);
 
-        // Assert
-        Assert.Equal("test value", activity.GetTagItem("tagged_property"));
-        Assert.Equal("42", activity.GetTagItem("prefix.prefixed_property"));
-        Assert.Null(activity.GetTagItem("UntaggedProperty"));
+        Assert.Equal("123", activity.GetTagItem("user_id"));
+        Assert.Equal("John Doe", activity.GetTagItem("user.name"));
+        Assert.Null(activity.GetTagItem("Email")); // Email should not be tagged
     }
 
     [Fact]
-    public void SetTagsFromObject_WithNullValues_ShouldNotSetTags()
+    public void AddTagsForProductInfo_ShouldAddCorrectTags()
     {
-        // Arrange
-        var testObject = new TestClass
+        var product = new ProductInfo
         {
-            TaggedProperty = null,
-            PrefixedProperty = 42,
-            UntaggedProperty = "should not be tagged"
+            Id = "PROD-001",
+            Price = 29.99m
         };
-
         var activity = new Activity("TestActivity");
         activity.Start();
 
-        // Act
-        activity.SetTagsFromObject(testObject);
+        activity.AddActionTagsForProductInfo(product);
 
-        // Assert
-        Assert.Null(activity.GetTagItem("tagged_property"));
-        Assert.Equal("42", activity.GetTagItem("prefix.prefixed_property"));
+        Assert.Equal("PROD-001", activity.GetTagItem("product_id"));
+        Assert.Equal("29.99", activity.GetTagItem("price"));
     }
 
     [Fact]
-    public void SetTagsFromObject_WithNullObject_ShouldNotThrowException()
+    public void AddTags_WithNullObject_ShouldNotThrowException()
     {
-        // Arrange
-        TestClass testObject = null;
+        UserInfo user = null;
         var activity = new Activity("TestActivity");
         activity.Start();
-
-        // Act & Assert
-        var exception = Record.Exception(() => activity.SetTagsFromObject(testObject));
+        
+        var exception = Record.Exception(() => activity.AddActionTagsForUserInfo(user));
         Assert.Null(exception);
     }
-}
 
-public class TestClass
-{
-    [ActionTag("tagged_property")]
-    public string? TaggedProperty { get; set; }
+    [Fact]
+    public void AddTags_WithNullProperties_ShouldNotAddTags()
+    {
+        var user = new UserInfo(); // All properties will be null or default
+        var activity = new Activity("TestActivity");
+        activity.Start();
+        
+        activity.AddActionTagsForUserInfo(user);
 
-    [ActionTag("prefixed_property", "prefix")]
-    public int PrefixedProperty { get; set; }
-
-    public string? UntaggedProperty { get; set; }
+        Assert.Null(activity.GetTagItem("user_id"));
+        Assert.Null(activity.GetTagItem("user.name"));
+    }
 }
