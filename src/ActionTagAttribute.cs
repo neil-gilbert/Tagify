1a31284dc8c082022c29e6ce71511f38c7f4d015
Neil Gilbert <neil_gilbert_1234@hotmail.com>
2024-09-18 19:28:44 +0100

    Add class level prefix

--- src/ActionTagAttribute.cs
+++ src/ActionTagAttribute.cs
@@ -1,9 +1,9 @@
 namespace Tagify;
 
 /// <summary>
-/// Attribute to specify OpenTelemetry tag details for a property.
+/// Attribute to specify OpenTelemetry tag details for a property or class.
 /// </summary>
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 public class ActionTagAttribute : Attribute
 {
     /// <summary>
@@ -19,8 +19,8 @@ public class ActionTagAttribute : Attribute
     /// <summary>
     /// Initializes a new instance of the <see cref="ActionTagAttribute"/> class.
     /// </summary>
-    /// <param name="name">The name of the tag.</param>
-    /// <param name="prefix">The prefix for the tag.</param>
+    /// <param name="name">The name of the tag. If not provided for a class, all properties will be tagged.</param>
+    /// <param name="prefix">The prefix for the tag. If set on a class, it applies to all tagged properties unless overridden.</param>
     public ActionTagAttribute(string? name = null, string? prefix = null)
     {
         Name = name;
