9a36ffdcfaeff2b9596f050f4ffdc61dd41bf8b5
Neil Gilbert <neil_gilbert_1234@hotmail.com>
2024-10-06 14:12:17 +0100

    Support nested objects in gen

--- src/Tagify.Generator/ActionTagGenerator.cs
+++ src/Tagify.Generator/ActionTagGenerator.cs
@@ -1,8 +1,9 @@
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using System.Text;
 
-namespace Tagify.Generator;
+namespace Tagify;
 
 [Generator]
 public class ActionTagGenerator : ISourceGenerator
@@ -11,45 +12,26 @@ public class ActionTagGenerator : ISourceGenerator
     
     public void Initialize(GeneratorInitializationContext context)
     {
-#pragma warning disable RS1035
         context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
-#pragma warning restore RS1035
     }
 
     public void Execute(GeneratorExecutionContext context)
     {
-#pragma warning disable RS1035
         if (!(context.SyntaxContextReceiver is SyntaxReceiver receiver))
-#pragma warning restore RS1035
             return;
 
-        foreach (var typeSymbol in receiver.CandidateTypes)
+        foreach (var classSymbol in receiver.CandidateClasses)
         {
-            var classSource = GenerateExtensionMethod(typeSymbol);
-#pragma warning disable RS1035
-            context.AddSource($"{typeSymbol.Name}_ActionTags.g.cs", SourceText.From(classSource, Encoding.UTF8));
-#pragma warning restore RS1035
+            var classSource = GenerateExtensionMethod(classSymbol);
+            context.AddSource($"{classSymbol.Name}_ActionTags.g.cs", SourceText.From(classSource, Encoding.UTF8));
         }
     }
 
-    private string GenerateExtensionMethod(INamedTypeSymbol typeSymbol)
+    private string GenerateExtensionMethod(INamedTypeSymbol classSymbol)
     {
-        var namespaceName = typeSymbol.ContainingNamespace.ToDisplayString();
-        var typeName = typeSymbol.Name;
-        var methodName = $"AddActionTagsFor{typeName}";
-
-        var classAttribute = typeSymbol.GetAttributes()
-            .FirstOrDefault(a => a.AttributeClass?.Name == "ActionTagAttribute");
-        var classPrefix = classAttribute?.ConstructorArguments.Length > 1 
-            ? classAttribute?.ConstructorArguments[1].Value?.ToString() 
-            : null;
-
-        var taggedProperties = typeSymbol.GetMembers()
-            .OfType<IPropertySymbol>()
-            .Where(p => p.DeclaredAccessibility == Accessibility.Public &&
-                        p.Name != "EqualityContract" &&
-                        (classAttribute != null || p.GetAttributes().Any(a => a.AttributeClass?.Name == "ActionTagAttribute")))
-            .ToList();
+        var namespaceName = classSymbol.ContainingNamespace.ToDisplayString();
+        var className = classSymbol.Name;
+        var methodName = $"AddActionTagsFor{className}";
 
         var sourceBuilder = new StringBuilder();
         sourceBuilder.AppendLine($@"
@@ -58,83 +40,98 @@ using System.Collections.Generic;
 
 namespace {namespaceName}
 {{
-    public static class {typeName}ActionExtensions
+    public static class {className}ActionExtensions
     {{
-        public static Activity {methodName}(this Activity activity, {typeName} obj, IEnumerable<KeyValuePair<string, object?>>? additionalTags = null)
+        public static Activity {methodName}(this Activity activity, {className} obj, string prefix = """", IEnumerable<KeyValuePair<string, object?>>? additionalTags = null)
         {{
             if (activity == null || obj == null) return activity;
-");
 
-        foreach (var property in taggedProperties)
-        {
-            var attribute = property.GetAttributes().FirstOrDefault(a => a.AttributeClass?.Name == "ActionTagAttribute");
-            var tagName = attribute?.ConstructorArguments.Length > 0 && attribute.ConstructorArguments[0].Value != null
-                ? attribute.ConstructorArguments[0].Value?.ToString()
-                : property.Name.ToLowerInvariant();
+            {GeneratePropertyTaggingCode(classSymbol, "obj", "prefix")}
 
-            var propertyPrefix = attribute?.ConstructorArguments.Length > 1 && attribute.ConstructorArguments[1].Value != null
-                ? attribute.ConstructorArguments[1].Value?.ToString()
-                : classPrefix;
-
-            var fullTagName = string.IsNullOrEmpty(propertyPrefix) ? tagName : $"{propertyPrefix}.{tagName}";
-
-            sourceBuilder.AppendLine($@"            if (obj.{property.Name} != null)
-                activity.SetTag(""{fullTagName}"", obj.{property.Name});");
-        }
-
-        sourceBuilder.AppendLine(@"
             if (additionalTags != null)
-            {
+            {{
                 foreach (var tag in additionalTags)
-                {
+                {{
                     activity.SetTag(tag.Key, tag.Value);
-                }
-            }
+                }}
+            }}
 
             return activity;
-        }
-    }
-}");
+        }}
+    }}
+}}");
 
         return sourceBuilder.ToString();
+    }
+
+    private string GeneratePropertyTaggingCode(INamedTypeSymbol classSymbol, string objName, string prefixName)
+    {
+        var sourceBuilder = new StringBuilder();
+        var classAttribute = classSymbol.GetAttributes().FirstOrDefault(a => a.AttributeClass?.Name == ActionTagAttributeName);
+        var classPrefix = classAttribute?.ConstructorArguments.Length > 0 ? classAttribute.ConstructorArguments[0].Value?.ToString() : null;
+
+        foreach (var property in classSymbol.GetMembers().OfType<IPropertySymbol>())
+        {
+            var attribute = property.GetAttributes().FirstOrDefault(a => a.AttributeClass?.Name == ActionTagAttributeName);
+            if (attribute == null && !HasActionTagAttribute(property.Type)) continue;
+
+            var tagName = attribute?.ConstructorArguments.Length > 0 ? attribute.ConstructorArguments[0].Value?.ToString() : property.Name.ToLowerInvariant();
+            var propertyPrefix = attribute?.ConstructorArguments.Length > 1 ? attribute.ConstructorArguments[1].Value?.ToString() : null;
+
+            var fullTagName = BuildFullTagName(prefixName, classPrefix, propertyPrefix, tagName);
+
+            if (HasActionTagAttribute(property.Type))
+            {
+                sourceBuilder.AppendLine($@"            if ({objName}.{property.Name} != null)
+                {methodName}(activity, {objName}.{property.Name}, {fullTagName});");
+            }
+            else
+            {
+                sourceBuilder.AppendLine($@"            if ({objName}.{property.Name} != null)
+                activity.SetTag({fullTagName}, {objName}.{property.Name});");
+            }
+        }
+
+        return sourceBuilder.ToString();
+    }
+
+    private bool HasActionTagAttribute(ITypeSymbol type)
+    {
+        return type.GetAttributes().Any(a => a.AttributeClass?.Name == ActionTagAttributeName);
+    }
+
+    private string BuildFullTagName(string prefixName, string? classPrefix, string? propertyPrefix, string tagName)
+    {
+        var parts = new[] { $"{prefixName}", classPrefix, propertyPrefix, tagName }
+            .Where(p => !string.IsNullOrEmpty(p))
+            .Select(p => $"\"{p}\"");
+
+        return $"string.Join(\".\", new[] {{ {string.Join(", ", parts)} }}.Where(p => !string.IsNullOrEmpty(p)))";
     }
 }
 
 internal class SyntaxReceiver : ISyntaxContextReceiver
 {
-    public List<INamedTypeSymbol> CandidateTypes { get; } = new List<INamedTypeSymbol>();
+    public List<INamedTypeSymbol> CandidateClasses { get; } = new List<INamedTypeSymbol>();
 
     public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
     {
-        if (context.Node is ClassDeclarationSyntax classDeclaration)
-        {
-            ProcessTypeDeclaration(context, classDeclaration);
-        }
-        else if (context.Node is RecordDeclarationSyntax recordDeclaration)
-        {
-            ProcessTypeDeclaration(context, recordDeclaration);
-        }
-    }
-
-    private void ProcessTypeDeclaration(GeneratorSyntaxContext context, TypeDeclarationSyntax typeDeclaration)
-    {
-        if (context.SemanticModel.GetDeclaredSymbol(typeDeclaration) is not INamedTypeSymbol
-            { DeclaredAccessibility: Accessibility.Public } symbol)
-            return;
-
+        if (context.Node is not ClassDeclarationSyntax classDeclarationSyntax) return;
+        
+        if (context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax) is not INamedTypeSymbol
+            { DeclaredAccessibility: Accessibility.Public } symbol) return;
+        
         var hasClassAttribute = symbol.GetAttributes()
             .Any(attr => attr.AttributeClass?.Name == ActionTagGenerator.ActionTagAttributeName);
 
         var hasPropertyAttributes = symbol.GetMembers()
             .OfType<IPropertySymbol>()
-            .Any(prop => prop.DeclaredAccessibility == Accessibility.Public &&
-                         prop.Name != "EqualityContract" &&
-                         prop.GetAttributes()
-                             .Any(attr => attr.AttributeClass?.Name == ActionTagGenerator.ActionTagAttributeName));
+            .Any(prop => prop.GetAttributes()
+                .Any(attr => attr.AttributeClass?.Name == ActionTagGenerator.ActionTagAttributeName));
 
         if (hasClassAttribute || hasPropertyAttributes)
         {
-            CandidateTypes.Add(symbol);
+            CandidateClasses.Add(symbol);
         }
     }
 }
